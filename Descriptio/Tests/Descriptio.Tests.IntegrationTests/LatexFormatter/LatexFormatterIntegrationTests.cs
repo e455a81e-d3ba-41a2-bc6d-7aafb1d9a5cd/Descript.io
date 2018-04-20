1c681d411000cf07acaaddbeef47b6306f7db482
Leonhardt Schwarz <Leonhardt.Schwarz@zuehlke.com>
2018-04-20 15:18:31 +0200

    Actual integration test

--- Descriptio/Tests/Descriptio.Tests.IntegrationTests/LatexFormatter/LatexFormatterIntegrationTests.cs
+++ Descriptio/Tests/Descriptio.Tests.IntegrationTests/LatexFormatter/LatexFormatterIntegrationTests.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using Descriptio.Core.AST;
+using Descriptio.Parser;
 using Xunit;
 
 namespace Descriptio.Tests.IntegrationTests.LatexFormatter
@@ -14,74 +11,23 @@ namespace Descriptio.Tests.IntegrationTests.LatexFormatter
         [Fact]
         public void LatexFormatter_ShouldYieldDocument()
         {
-            var latexFormatter = new Descriptio.Transform.Latex.LatexFormatter();
 
-            var ast = new TitleAst(
-                "Title 1",
-                level: 1,
-                next: new TextParagraphBlock(new[]
-                    {
-                        new CleanTextInline("This is a text."),
-                    },
-                    next: new TitleAst(
-                        "Title 2",
-                        level: 2,
-                        next: new TextParagraphBlock(new IAbstractSyntaxTreeInline[]
-                        {
-                            new CleanTextInline("This is another text. "),
-                            new StrongTextInline("This should be strong."),
-                            new CleanTextInline(" "),
-                            new EmphasisTextInline("And this should be emphasized."),
-                            new CleanTextInline(" "),
-                            new CodeTextInline("This should be formatted as code."),
-                        },
-                            next: new TextParagraphBlock(new IAbstractSyntaxTreeInline[]
-                            {
-                                new CleanTextInline("Here, we should have a new paragraph "),
-                                new HyperlinkInline(text: "with a link", href: "http://example.com", title: "It is a title"),
-                                new CleanTextInline("."),
-                                new ImageInline(alt: "Alt", src: @"C:\Path\To\An\Image.jpg", title: "It has a title too"),
-                            },
-                                next: new EnumerationBlock(
-                                    items: new[]
-                                    {
-                                        new EnumerationItem(1, new[] { new CleanTextInline("This should be item 1.") }),
-                                        new EnumerationItem(2, new[] { new CleanTextInline("This should be the second item.")}),
-                                        new EnumerationItem(3, new[] { new CleanTextInline("Though, this should be item 3.")})
-                                    }
-                                )
-                            )
-                        )
-                )));
+            var markdown = "# Title 1\r\nThis is a text.\r\n**This should be strong.**\r\n*And this should be emphasized.*\r\n`This should be formatted as code.`\r\n\r\nHere, we should have a new paragraph\r\n[with a link](http://example.com \"It is a title\")\r\nand an image ![Alt](C:\\Path\\To\\An\\Image.jpg, \"It has a title too\")\r\n\r\n1. This should be item 1.\r\n2. This should be the second item.\r\n1234. Though, this should be item 3.";
 
-            var expectedResult = @"\section*{Title 1}
-\begin{paragraph}{}
-This is a text.
-\end{paragraph}
-\section*{Title 2}
-\begin{paragraph}{}
-This is another text. \textbf{This should be strong.} \emph{And this should be emphasized.} \begin{verbatim}
-This should be formatted as code.
-\end{verbatim}
+            var lexer = new MarkdownLexer.TextLexer();
+            var parser = new MarkdownParser.MarkdownParser();
 
-\end{paragraph}
-\begin{paragraph}{}
-Here, we should have a new paragraph \href{http://example.com}{with a link}.\begin{figure}[h]
-\centering
-\includegraphics{C:\Path\To\An\Image.jpg}
-\caption{It has a title too}
-\end{figure}
+            // Act & Assert
+            var lexerResult = lexer.Lex(markdown);
 
-\end{paragraph}
-\begin{itemize}
-\item [1] This should be item 1.
-\item [2] This should be the second item.
-\item [3] Though, this should be item 3.
-\end{itemize}
-";
+            var (_, _, _, output) = lexerResult.Value;
+            var parserResult = parser.Parse(output);
 
+            var latexFormatter = new Transform.Latex.LatexFormatter();
+
+            var expectedResult = "\\section*{Title 1}\r\n\\begin{paragraph}{}\r\nThis is a text. \\textbf{This should be strong.}\r \\emph{And this should be emphasized.}\r \\begin{verbatim}\r\nThis should be formatted as code.\r\n\\end{verbatim}\r\n\r\n\\end{paragraph}\r\n\\begin{paragraph}{}\r\nHere, we should have a new paragraph \\href{http://example.com}{with a link}\r and an image \\begin{figure}[h]\r\n\\centering\r\n\\includegraphics{C:\\Path\\To\\An\\Image.jpg,}\r\n\\caption{It has a title too}\r\n\\end{figure}\r\n\r\n\\end{paragraph}\r\n\\begin{itemize}\r\n\\item [1] This should be item 1.\r\n\\item [2] This should be the second item.\r\n\\item [1234] Though, this should be item 3.\r\n\\end{itemize}\r\n";
             var memoryStream = new MemoryStream();
-            latexFormatter.Transform(ast, memoryStream);
+            latexFormatter.Transform((IAbstractSyntaxTreeBlock)parserResult.Value, memoryStream);
 
             using (var streamReader = new StreamReader(memoryStream, Encoding.UTF8))
             {
