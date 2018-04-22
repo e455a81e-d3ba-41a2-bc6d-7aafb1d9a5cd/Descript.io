3063a1cfdb1e9641cc3c737376184778c64d16a9
leonhardt <lariscusObscurus@gmail.com>
2018-04-22 22:00:06 +0200

    Added xml formatter

---
+++ Descriptio/Descriptio.Transform/Xml/XmlAbstractSyntaxTreeInlineVisitor.cs
@@ -0,0 +1,84 @@
+﻿using System.Xml;
+using Descriptio.Core.AST;
+
+namespace Descriptio.Transform.Xml
+{
+    public class XmlAbstractSyntaxTreeInlineVisitor : IAbstractSyntaxTreeInlineVisitor
+    {
+        private readonly XmlWriter _xmlWriter;
+
+        public XmlAbstractSyntaxTreeInlineVisitor(XmlWriter xmlWriter)
+        {
+            _xmlWriter = xmlWriter;
+        }
+
+        public void Visit(CleanTextInline cleanTextInline)
+        {
+            _xmlWriter.WriteStartElement("clean_text");
+            _xmlWriter.WriteValue(cleanTextInline.Text);
+            _xmlWriter.WriteEndElement();
+        }
+
+        public void Visit(CodeTextInline codeTextInline)
+        {
+            _xmlWriter.WriteStartElement("code_text");
+            _xmlWriter.WriteValue(codeTextInline.Text);
+            _xmlWriter.WriteEndElement();
+        }
+
+        public void Visit(EmphasisTextInline emphasisTextInline)
+        {
+            _xmlWriter.WriteStartElement("emphasised_text");
+            _xmlWriter.WriteValue(emphasisTextInline.Text);
+            _xmlWriter.WriteEndElement();
+        }
+
+        public void Visit(HyperlinkInline hyperlinkInline)
+        {
+            _xmlWriter.WriteStartElement("hyperlink");
+
+            _xmlWriter.WriteStartElement("hyperlink_text");
+            _xmlWriter.WriteValue(hyperlinkInline.Text);
+            _xmlWriter.WriteEndElement();
+
+            _xmlWriter.WriteStartElement("hyperlink_href");
+            _xmlWriter.WriteValue(hyperlinkInline.Href);
+            _xmlWriter.WriteEndElement();
+
+            _xmlWriter.WriteEndElement();
+        }
+
+        public void Visit(ImageInline imageInline)
+        {
+            _xmlWriter.WriteStartElement("image");
+
+            _xmlWriter.WriteStartElement("image_src");
+            _xmlWriter.WriteValue(imageInline.Src);
+            _xmlWriter.WriteEndElement();
+
+            _xmlWriter.WriteStartElement("image_alt");
+            _xmlWriter.WriteValue(imageInline.Alt);
+            _xmlWriter.WriteEndElement();
+
+            _xmlWriter.WriteStartElement("image_title");
+            _xmlWriter.WriteValue(imageInline.Title);
+            _xmlWriter.WriteEndElement();
+
+            _xmlWriter.WriteEndElement();
+        }
+
+        public void Visit(StrongTextInline strongTextInline)
+        {
+            _xmlWriter.WriteStartElement("strong_text");
+            _xmlWriter.WriteValue(strongTextInline.Text);
+            _xmlWriter.WriteEndElement();
+        }
+
+        public void Visit(TextInline textInline)
+        {
+            _xmlWriter.WriteStartElement("text");
+            _xmlWriter.WriteValue(textInline.Text);
+            _xmlWriter.WriteEndElement();
+        }
+    }
+}
---
+++ Descriptio/Descriptio.Transform/Xml/XmlAbstractSyntaxTreeVisitor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using Descriptio.Core.AST;
+
+namespace Descriptio.Transform.Xml
+{
+    public class XmlAbstractSyntaxTreeVisitor : IAbstractSyntaxTreeVisitor
+    {
+        private readonly XmlWriter _xmlWriter;
+        private readonly XmlAbstractSyntaxTreeInlineVisitor _inlineVisitor;
+
+        public XmlAbstractSyntaxTreeVisitor(XmlWriter xmlWriter)
+        {
+            _xmlWriter = xmlWriter;
+            _inlineVisitor = new XmlAbstractSyntaxTreeInlineVisitor(xmlWriter);
+        }
+
+        public void Visit(TitleAst titleAst)
+        {
+            _xmlWriter.WriteStartElement("title");
+            _xmlWriter.WriteValue(titleAst.Text);
+            _xmlWriter.WriteEndElement();
+        }
+
+        public void Visit(TextParagraphBlock textParagraphBlock)
+        {
+            _xmlWriter.WriteStartElement("text_paragraph");
+            FormatInlines(textParagraphBlock.Inlines);
+            _xmlWriter.WriteEndElement();
+        }
+
+        public void Visit(EnumerationBlock enumerationBlock)
+        {
+            _xmlWriter.WriteStartElement("enumeration");
+            foreach (var item in enumerationBlock.Items)
+            {
+                _xmlWriter.WriteStartElement("item");
+
+                _xmlWriter.WriteStartAttribute("number");
+                _xmlWriter.WriteValue(item.Number);
+                _xmlWriter.WriteEndAttribute();
+
+                FormatInlines(item.Inlines);
+                _xmlWriter.WriteEndElement();
+            }
+            _xmlWriter.WriteEndElement();
+        }
+
+        private void FormatInlines(IEnumerable<IAbstractSyntaxTreeInline> inlines)
+        {
+            foreach (var inline in inlines)
+            {
+                inline.Accept(_inlineVisitor);
+            }
+        }
+    }
+}
