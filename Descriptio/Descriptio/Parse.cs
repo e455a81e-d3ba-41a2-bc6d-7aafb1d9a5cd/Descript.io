f74fd88167cfd8abd9cfb652e585e7775fcc05fe
David Eiwen <david-e-winlive@live.at>
2018-04-24 09:19:47 +0200

    Added Xmldoc for fluent API

--- Descriptio/Descriptio/Parse.cs
+++ Descriptio/Descriptio/Parse.cs
@@ -9,17 +9,49 @@ using static Descriptio.Parser.MarkdownLexer;
 
 namespace Descriptio
 {
+    /// <summary>
+    /// Provides the fluent API for parsing.
+    /// </summary>
     public static class Parse
     {
         private static readonly LexerFactory LexerFactory = new LexerFactory();
         private static readonly ParserFactory ParserFactory = new ParserFactory();
 
+        /// <summary>
+        /// Parses a markdown string using the default lexer and parser implementations.
+        /// </summary>
+        /// <param name="input">
+        /// The markdown string to parse.
+        /// </param>
+        /// <returns>The result of the parsing process.</returns>
         public static ParseResult MarkdownString(string input)
             => MarkdownStringUsing(
                 LexerFactory.CreateMarkdownTextLexerWithDefaultRules(),
                 ParserFactory.CreateMarkdownParserWithDefaultRules(),
                 input);
 
+        /// <summary>
+        /// Parses a markdown string using a specified lexer and parser.
+        /// </summary>
+        /// <param name="lexer">
+        /// The lexer to use.
+        /// Must not be <c>null</c>.
+        /// </param>
+        /// <param name="parser">
+        /// The parser to use.
+        /// Must not be <c>null</c>.
+        /// </param>
+        /// <param name="input">
+        /// The markdown string to parse.
+        /// </param>
+        /// <returns>The result of the parsing process.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if
+        /// <list type="bullet">
+        /// <item><paramref name="lexer"/> is <c>null</c>.</item>
+        /// <item><paramref name="parser"/> is <c>null</c>.</item>
+        /// </list>
+        /// </exception>
         public static ParseResult MarkdownStringUsing(ILexer<string, IEnumerable<Token>> lexer, IParser<IEnumerable<Token>> parser, string input)
         {
             if (lexer is null) throw new ArgumentNullException(nameof(lexer));
--- Descriptio/Descriptio/ParseResult.cs
+++ Descriptio/Descriptio/ParseResult.cs
@@ -8,10 +8,23 @@ using Microsoft.FSharp.Core;
 
 namespace Descriptio
 {
+    /// <summary>
+    /// Represents the result of a parsing process.
+    /// </summary>
     public class ParseResult
     {
         private readonly FormatterFactory _formatterFactory = new FormatterFactory();
-
+        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ParseResult"/> class.
+        /// </summary>
+        /// <param name="parserResult">
+        /// The result of the parsing process, encapsulated in an <see cref="FSharpOption{T}"/> instance.
+        /// Must not be <c>null</c>.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="parserResult"/> is <c>null</c>.
+        /// </exception>
         public ParseResult(FSharpOption<IAbstractSyntaxTreeBlock> parserResult)
         {
             ParserResult = parserResult ?? throw new ArgumentNullException(nameof(parserResult));
@@ -19,14 +32,56 @@ namespace Descriptio
 
         public FSharpOption<IAbstractSyntaxTreeBlock> ParserResult { get; }
 
+        /// <summary>
+        /// Formats the result to a LaTex string using the default formatter.
+        /// </summary>
+        /// <returns>The LaTex string.</returns>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown if the <see cref="ParserResult"/> is <see cref="FSharpOption{T}.None"/>.
+        /// </exception>
         public string AndFormatToLaTexString() => AndFormatToStringUsing(_formatterFactory.CreateLaTexFormatterWithDefaultRules());
 
+        /// <summary>
+        /// Formats the result to an HTML string using the default formatter.
+        /// </summary>
+        /// <returns>The HTML string.</returns>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown if the <see cref="ParserResult"/> is <see cref="FSharpOption{T}.None"/>.
+        /// </exception>
         public string AndFormatToHtmlString() => AndFormatToStringUsing(_formatterFactory.CreateHtmlFormatterWithDefaultRules());
 
+        /// <summary>
+        /// Formats the result to an XML string using the default formatter.
+        /// </summary>
+        /// <returns>The XML string.</returns>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown if the <see cref="ParserResult"/> is <see cref="FSharpOption{T}.None"/>.
+        /// </exception>
         public string AndFormatToXmlString() => AndFormatToStringUsing(_formatterFactory.CreateXmlFormatterWithDefaultRules());
 
+        /// <summary>
+        /// Formats the result to a JSON string using the default formatter.
+        /// </summary>
+        /// <returns>The JSON string.</returns>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown if the <see cref="ParserResult"/> is <see cref="FSharpOption{T}.None"/>.
+        /// </exception>
         public string AndFormatToJsonString() => AndFormatToStringUsing(_formatterFactory.CreateJsonFormatterWithDefaultRules());
 
+        /// <summary>
+        /// Formats the result using a specified formatter-
+        /// </summary>
+        /// <param name="formatter">
+        /// The formatter to use.
+        /// Must not be <c>null</c>.
+        /// </param>
+        /// <returns>The formatted string.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="formatter"/> is <c>null</c>.
+        /// </exception>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown if the <see cref="ParserResult"/> is <see cref="FSharpOption{T}.None"/>.
+        /// </exception>
         public string AndFormatToStringUsing(IFormatter formatter)
         {
             ThrowIfCannotFormat();
@@ -48,6 +103,12 @@ namespace Descriptio
             }
         }
 
+        /// <summary>
+        /// Throws an <see cref="InvalidOperationException"/> if the parser result is <see cref="FSharpOption{T}.None"/>
+        /// </summary>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown if the value of <see cref="ParserResult"/> is <see cref="FSharpOption{T}.None"/>.
+        /// </exception>
         protected void ThrowIfCannotFormat()
         {
             if (ParserResult.IsNone())
